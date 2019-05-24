07ed5fb62db13501ada1393cc6d41827989190c3
TheJSGirl <prinegi100@gmail.com>
2019-05-24 12:44:14 +0530

    created Friend component

---
+++ friends.js
@@ -0,0 +1,10 @@
+class Friend extends React.Component{
+
+    render() {
+       
+      return (  <div>
+            <h1>Friends List</h1>
+        </div>)
+                                                                                                                                                                                                                                                                                                                                            
+    }
+} 
