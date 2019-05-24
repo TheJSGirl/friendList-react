4a5b993db2d7b82dbcaabf683b1c7c9d4fbd7cb6
TheJSGirl <prinegi100@gmail.com>
2019-05-24 15:52:28 +0530

    styling in react

--- friends.js
+++ friends.js
@@ -6,13 +6,13 @@ class Friend extends React.Component{
     render() {
        const {name, hobby, socialLinks} = this.props;
       return (  
-      <div>
-          <h2>{name}</h2>
-          <p><h3>Hobbies</h3>
+      <div className="Friend"> 
+          <h2 className="name">{name}</h2>
+          <p><h3 className="hobby">Hobbies</h3>
           {hobby.map((h, i) =><p>{h}</p>)}
           </p>
           <p>
-              <h3>Social Links</h3>
+              <h3 className="social">Social Links</h3>
               {socialLinks.map((s, i) =>{
                  return <div>
                        <p>Github: {s.github}</p>
